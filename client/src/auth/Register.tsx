217dc11354282ef31d2c3c5dd857940d547c3790
Donald Iloekwe <donytex4u@gmail.com>
2022-11-08 14:32:14 +0000

    fix width for register form

--- client/src/auth/Register.tsx
+++ client/src/auth/Register.tsx
@@ -74,7 +74,8 @@ const Register = () => {
   return (
     <>
       <Flex align={'center'} justify={'center'}>
-        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}
+          w={{ sm: 'full', md: '60vh' }}>
           <Stack align={'center'}>
             <Heading fontSize={'3xl'} color={'orange.500'}>
               Register an Account
